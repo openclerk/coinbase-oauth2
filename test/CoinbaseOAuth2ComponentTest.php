eb21132fddc741238f0106e3fdf595cd2ad4f54e
Jevon Wright <jevon@jevon.org>
2015-04-09 16:25:44 +1200

    Initial component structure

---
+++ test/CoinbaseOAuth2ComponentTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Openclerk\OAuth2\Client\Test;
+
+class CoinbaseOAuth2ComponentTest extends \ComponentTests\ComponentTest {
+
+  function getRoots() {
+    return array(__DIR__ . "/..");
+  }
+
+}
